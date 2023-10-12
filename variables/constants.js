4dc89e42ad9a30fa585cbacbc65a4bee1b838871
Mikko Huttunen <mikkohuttunen.92@gmail.com>
2023-10-12 22:12:37 +0300

    Fix midnight & midday constants

--- variables/constants.js
+++ variables/constants.js
@@ -54,8 +54,8 @@ export const INVALID_REPEAT = "Given repeat interval is invalid! Accepted values
     "weekly = Repeat every week\n" +
     "monthly = Repeat every month\n" +
     "yearly = Repeat every year";
-export const MIDNIGHT = "21:04";
-export const MIDDAY = "21:05";
+export const MIDNIGHT = "00:00";
+export const MIDDAY = "12:00";
 
 //Formatting
 export const EMPTY = " ";
